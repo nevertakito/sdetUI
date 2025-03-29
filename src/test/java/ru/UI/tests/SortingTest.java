13686a226aecc1b2719bb1db1db02121c07fc6cb
benever <thetakky@mail.ru>
2025-03-29 21:22:43 +0300

    changing the structure of Test classes

--- src/test/java/ru/UI/tests/SortingTest.java
+++ src/test/java/ru/UI/tests/SortingTest.java
@@ -1,37 +1,19 @@
 package ru.UI.tests;
 
 import io.qameta.allure.Description;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 import ru.UI.helpers.DriverPool;
-import ru.UI.pages.CustomersPage;
-import ru.UI.helpers.ConfProperties;
 
 import java.util.List;
 
 import static org.testng.AssertJUnit.assertEquals;
 
-public class SortingTest {
-    public static CustomersPage customersPage;
-    @BeforeClass
-    private void setup() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1080");
-        WebDriver driver = new ChromeDriver(options);
-        DriverPool.setDriver(driver);
-        customersPage = new CustomersPage(driver);
-
-        driver.manage().window().maximize();
-        driver.get(ConfProperties.getProperty("page"));
-    }
-
+public class SortingTest extends BaseTest{
     @Test
     @Description("Sorting the customers")
-    public void sortingTest() {
+    public final void sortingTest() {
+
         customersPage.clickCustomersBtn();
         List<String> list = customersPage.getArrayValues()
                 .stream()
@@ -41,9 +23,8 @@ public class SortingTest {
         List<String> sortedList = customersPage.getArrayValues();
         assertEquals(list, sortedList);
     }
-
-    @AfterClass
-    private void close(){
-        DriverPool.quitDriver();
+    @AfterMethod
+    public final void cache(){
+        DriverPool.getDriver().manage().deleteAllCookies();
     }
 }
